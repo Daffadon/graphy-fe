7dfbb452644c00617ea70065ae66a3fa673abfaa
daffadon <daffaputranarendra9@gmail.com>
2025-09-09 16:40:29 +0700

    refactor(codegen): update codegen to use react query plugin

--- codegen.ts
+++ codegen.ts
@@ -1,20 +1,24 @@
 import type { CodegenConfig } from "@graphql-codegen/cli";
 
 const config: CodegenConfig = {
-  schema: "https://graphql.org/graphql/",
-  documents: ["src/**/*.tsx"],
+  schema: "./schema.graphql",
+  documents: ["src/**/**/*.graphql"],
   ignoreNoDocuments: true,
   generates: {
-    "./src/graphql/": {
-      preset: "client",
+    "./src/graphql/client-generated.ts": {
+      plugins: [
+        "typescript",
+        "typescript-operations",
+        "typescript-react-query",
+      ],
       config: {
         documentMode: "string",
-      },
-    },
-    "./schema.graphql": {
-      plugins: ["schema-ast"],
-      config: {
-        includeDirectives: true,
+        useTypeImports: true,
+        addInfiniteQuery: true,
+        fetcher: "fetch",
+        exposeQueryKeys: true,
+        exposeFetcher: true,
+        reactQueryVersion: 5,
       },
     },
   },
