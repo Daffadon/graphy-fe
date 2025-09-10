ad5e6111eea662d53e666595a2ad92c8eee57112
daffadon <daffaputranarendra9@gmail.com>
2025-09-10 16:28:41 +0700

    feat(provider): add TanstackProvider for React Query integration

---
+++ src/lib/provider/TanstackProvider.tsx
@@ -0,0 +1,11 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { type ReactNode } from "react";
+
+const TanstackProvider = ({ children }: { children: ReactNode }) => {
+  const queryClient = new QueryClient();
+  return (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  );
+};
+
+export default TanstackProvider;
--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,13 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import Router from "./routes/routes.tsx";
 import { Toaster } from "./components/ui/sonner.tsx";
+import TanstackProvider from "./lib/provider/TanstackProvider.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Router />
-    <Toaster position="top-center" />
+    <TanstackProvider>
+      <Router />
+      <Toaster position="top-center" />
+    </TanstackProvider>
   </StrictMode>
 );
