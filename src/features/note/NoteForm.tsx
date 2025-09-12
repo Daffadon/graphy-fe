d5253f3e50bf33f3469d8ce905324fdc7cc94994
daffadon <daffaputranarendra9@gmail.com>
2025-09-12 10:12:56 +0700

    refactor(NoteForm): replace NoteDialog trigger with Button for improved dialog control

--- src/features/note/NoteForm.tsx
+++ src/features/note/NoteForm.tsx
@@ -1,9 +1,20 @@
 import NoteDialog from "@/components/pages/note/NoteDialog";
+import { Button } from "@/components/ui/button";
+import { useState } from "react";
 
 const NoteForm = () => {
+  const [openDialog, setOpenDialog] = useState<boolean>(false);
   return (
     <div className="flex justify-center items-center h-full">
-      <NoteDialog withTrigger={true} triggerLabel="+" />
+      <Button
+        className="cursor-pointer"
+        onClick={() => {
+          setOpenDialog(true);
+        }}
+      >
+        +
+      </Button>
+      <NoteDialog open={openDialog} setOpen={setOpenDialog} />
     </div>
   );
 };
