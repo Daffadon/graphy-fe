0b834153b5876bfcbe4f61df34a10be9fdc31c35
daffadon <daffaputranarendra9@gmail.com>
2025-09-12 13:22:57 +0700

    refactor(notelist): refactor to use custom hook for note fetching logic

--- src/features/note/NoteList.tsx
+++ src/features/note/NoteList.tsx
@@ -1,31 +1,9 @@
 import NoteCard from "@/components/pages/note/NoteCard";
 import { Skeleton } from "@/components/ui/skeleton";
-import { useNotesQuery } from "@/graphql/client-generated";
-import Cookies from "js-cookie";
-import { useNavigate } from "react-router";
-import { toast } from "sonner";
+import { useNoteList } from "./notelist.hook";
 
 const NoteList = () => {
-  const navigate = useNavigate();
-  const token = Cookies.get("ACCESS_TOKEN");
-  const { data, isLoading, isFetching, error } = useNotesQuery(
-    {
-      endpoint: import.meta.env.VITE_GRAPHQL_ENDPOINT,
-      fetchParams: {
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${token}`,
-        },
-      },
-    },
-    {},
-    { retry: 0, staleTime: 60000 }
-  );
-
-  if (isFetching) {
-    toast.loading("wait a moment", { id: "get-notelist" });
-  }
-
+  const { data, isLoading } = useNoteList();
   if (isLoading) {
     return (
       <div className="grid grid-cols-4 gap-2 place-items-center [grid-auto-rows:200px]">
@@ -34,23 +12,6 @@ const NoteList = () => {
         ))}
       </div>
     );
-  }
-  if (data) {
-    toast.dismiss("get-notelist");
-  }
-  if (error) {
-    const typedError = error as Error & { response?: { status?: number } };
-    if (
-      typeof typedError.message === "string" &&
-      (typedError.message === "Invalid token" ||
-        typedError?.response?.status === 403)
-    ) {
-      Cookies.remove("ACCESS_TOKEN");
-      toast.error("Session Ended!", {
-        description: "Login to Working With Your Notes",
-      });
-      navigate("/login");
-    }
   }
   return (
     <>
---
+++ src/features/note/notelist.hook.tsx
@@ -0,0 +1,45 @@
+import { useNotesQuery } from "@/graphql/client-generated";
+import Cookies from "js-cookie";
+import { useNavigate } from "react-router";
+import { toast } from "sonner";
+
+export const useNoteList = () => {
+  const navigate = useNavigate();
+  const token = Cookies.get("ACCESS_TOKEN");
+  const { data, isLoading, isFetching, error } = useNotesQuery(
+    {
+      endpoint: import.meta.env.VITE_GRAPHQL_ENDPOINT,
+      fetchParams: {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      },
+    },
+    {},
+    { retry: 0, staleTime: 60000 }
+  );
+
+  if (isFetching) {
+    toast.loading("wait a moment", { id: "get-notelist" });
+  }
+
+  if (data) {
+    toast.dismiss("get-notelist");
+  }
+  if (error) {
+    const typedError = error as Error & { response?: { status?: number } };
+    if (
+      typeof typedError.message === "string" &&
+      (typedError.message === "Invalid token" ||
+        typedError?.response?.status === 403)
+    ) {
+      Cookies.remove("ACCESS_TOKEN");
+      toast.error("Session Ended!", {
+        description: "Login to Working With Your Notes",
+      });
+      navigate("/login");
+    }
+  }
+  return { data, isLoading };
+};
