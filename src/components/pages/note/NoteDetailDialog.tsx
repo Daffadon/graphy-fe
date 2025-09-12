19d511759c2af1384cbf58bb1696219f1f3408a6
daffadon <daffaputranarendra9@gmail.com>
2025-09-12 10:13:29 +0700

    feat(NoteDetailDialog): fetch note details from GraphQL and display in dialog

--- src/components/pages/note/NoteDetailDialog.tsx
+++ src/components/pages/note/NoteDetailDialog.tsx
@@ -8,24 +8,48 @@ import {
   DialogHeader,
   DialogTitle,
 } from "@/components/ui/dialog";
-import type { Dispatch } from "react";
+import { useNoteQuery } from "@/graphql/client-generated";
+import Cookies from "js-cookie";
+import { type Dispatch } from "react";
+import { toast } from "sonner";
 
 interface INoteDetailDialog {
   open: boolean;
   setOpen: Dispatch<React.SetStateAction<boolean>>;
   updateHandler: () => void;
-  title: string;
-  description: string;
-  content: string;
+  noteid: string;
 }
 const NoteDetailDialog = ({
   open,
   setOpen,
   updateHandler,
-  title,
-  description,
-  content,
+  noteid,
 }: INoteDetailDialog) => {
+  const token = Cookies.get("ACCESS_TOKEN");
+  const { data, isFetching } = useNoteQuery(
+    {
+      endpoint: import.meta.env.VITE_GRAPHQL_ENDPOINT,
+      fetchParams: {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      },
+    },
+    { id: noteid },
+    {
+      staleTime: 1000 * 60 * 5,
+      queryKey: ["Note", noteid],
+    }
+  );
+  if (isFetching) {
+    toast.loading("wait a moment", { id: "loading-toast" });
+  }
+
+  if (data) {
+    toast.dismiss("loading-toast");
+  }
+
   return (
     <Dialog
       open={open}
@@ -39,11 +63,11 @@ const NoteDetailDialog = ({
     >
       <DialogContent className="sm:max-w-xl">
         <DialogHeader>
-          <DialogTitle>{title}</DialogTitle>
-          <DialogDescription>{description}</DialogDescription>
+          <DialogTitle>{data?.note?.title}</DialogTitle>
+          <DialogDescription>{data?.note?.description}</DialogDescription>
         </DialogHeader>
         <div className="grid gap-4">
-          <p className="text-md">{content}</p>
+          <p className="text-md">{data?.note?.text}</p>
           <DialogFooter>
             <DialogClose asChild>
               <Button variant="outline" className="cursor-pointer">
